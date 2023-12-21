4e39d0421bef0ca9f6c4853ea8eddb7b50529a9b
양유빈 <102217712+YangYubin12@users.noreply.github.com>
2023-12-22 07:46:27 +0900

    Create chap201.c

---
+++ chapter2/chap201.c
@@ -0,0 +1,14 @@
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    if(mkdir("han", 0755) == -1){
+        perror("han");
+        exit(1);
+    }
+
+    printf("success\n");
+
+    return 0;
+}
